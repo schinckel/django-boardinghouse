18e764acd0c94368a6fbc288bcba30aa797214fa
Matthew Schinckel <matt@schinckel.net>
2012-05-02 23:37:33 +0930

    Automatically create the schema when it is first saved. Add in help text for admin.

--- multi_schema/models.py
+++ multi_schema/models.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models, connection
+from django.utils.translation import ugettext as _
 
 models.Model._is_schema_aware = False
 
 class Schema(models.Model):
-    name = models.CharField(max_length=128)
-    schema = models.CharField(max_length=36)
+    name = models.CharField(max_length=128, help_text=_(u'The display name of the schema.'))
+    schema = models.CharField(max_length=36, unique=True, 
+        help_text=_(u'The internal name of the schema. May not be changed after creation.'))
     
     class Meta:
         app_label = 'multi_schema'
@@ -14,8 +16,14 @@ class Schema(models.Model):
     def __unicode__(self):
         return self.name
     
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.create_schema()
+        return super(Schema, self).save(*args, **kwargs)
+    
     def create_schema(self):
         connection.cursor().execute('CREATE SCHEMA "%s";' %self.schema)
+        # We probably also want to create any tables that are required?
     
     def activate(self):
         connection.cursor().execute('SET search_path TO "%s",public' % self.schema)
