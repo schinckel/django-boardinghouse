c58e3c207ad5f534ea8a7e17cb13f6a1a1b8c714
Matthew Schinckel <matt@schinckel.net>
2012-05-02 23:36:47 +0930

    Make 'schema' value readonly after creation. Inject SchemaUser into UserAdmin inlines.

--- multi_schema/admin.py
+++ multi_schema/admin.py
@@ -1,7 +1,22 @@
-from django.contrib import admin
-from models import Schema
+from django.contrib import admin, auth
+from models import Schema, UserSchema
 
 class SchemaAdmin(admin.ModelAdmin):
-    pass
+    def get_readonly_fields(self, request, obj=None):
+        if obj is not None:
+            return ('schema',)
+        return ()
 
 admin.site.register(Schema, SchemaAdmin)
+
+class SchemaInline(admin.StackedInline):
+    model = UserSchema
+
+# Inject SchemeInline into UserAdmin
+UserAdmin = admin.site._registry[auth.models.User].__class__
+
+class SchemaUserAdmin(UserAdmin):
+    inlines = UserAdmin.inlines + [SchemaInline]
+    
+admin.site.unregister(auth.models.User)
+admin.site.register(auth.models.User, SchemaUserAdmin)
