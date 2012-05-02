58d73429952a942d03b232242424946895ec3e8c
Matthew Schinckel <matt@schinckel.net>
2012-05-02 23:38:28 +0930

    Add some data into the request context. Better handling of missing Schema objects when logging in (should we raise an error?).

--- multi_schema/middleware.py
+++ multi_schema/middleware.py
@@ -7,18 +7,28 @@ Otherwise, set the schema to the one associated with the logged in user.
 
 
 """
+from django.core.exceptions import ObjectDoesNotExist
+
 from models import Schema
 
 class SchemaMiddleware:
     def process_request(self, request):
         if request.user.is_anonymous():
             return None
-        if request.user.is_superuser and '__schema' in request.GET:
-            request.session['schema'] = request.GET['__schema']
-        if request.user.is_superuser and 'schema' in request.session:
-            Schema.objects.get(pk=request.session['schema']).activate()
+        if request.user.is_superuser:
+            if '__schema' in request.GET:
+                request.session['schema'] = request.GET['__schema']
+            if 'schema' in request.session:
+                Schema.objects.get(pk=request.session['schema']).activate()
         else:
-            request.user.schema.schema.activate()
+            try:
+                request.user.schema.schema.activate()
+            except ObjectDoesNotExist:
+                pass
+            
     
-    def process_response(self, request):
-        pass
+    def process_template_response(self, request, response):
+        if request.user.is_superuser:
+            response.context_data['schemata'] = Schema.objects.all()
+            response.context_data['selected_schema'] = request.session['schema']
+        return response
