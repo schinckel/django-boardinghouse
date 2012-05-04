9d78b571fcd0575e02d4849a0938a51f15e07961
Matthew Schinckel <matt@schinckel.net>
2012-05-04 17:19:38 +0930

    Create the correct references to other tables in all schemata.

--- multi_schema/management/__init__.py
+++ multi_schema/management/__init__.py
@@ -7,16 +7,27 @@ def post_syncdb_duplicator(sender, **kwargs):
     # See if any of the newly created models are schema-aware
     schema_aware_models = [m for m in kwargs['created_models'] if m._is_schema_aware and kwargs['app'].__name__ == m.__module__]
     if schema_aware_models:
-        import pdb; pdb.set_trace()
         for schema in Schema.objects.all():
             cursor = connection.cursor()
+            schema.activate(cursor)
             tables = connection.introspection.table_names()
+            pending_references = {}
             known_models = set([model for model in connection.introspection.installed_models(tables)])
+            seen_models = set(known_models)
             for model in schema_aware_models:
                 output, references = connection.creation.sql_create_model(model, no_style(), known_models, schema.schema)
+                seen_models.add(model)
+                for refto, refs in references.items():
+                    pending_references.setdefault(refto, []).extend(refs)
+                    if refto in seen_models:
+                        output.extend(connection.creation.sql_for_pending_references(refto, no_style(), pending_references))
+                output.extend(connection.creation.sql_for_pending_references(model, no_style(), pending_references))
+                
                 if output and kwargs.get('verbosity', 0) > 1:
-                    print "Creating table %s in schema %s" % (model._meta.db_table, schema.schema)
+                    print " ... creating table %s in schema %s" % (model._meta.db_table, schema.schema)
                 for statement in output:
                     cursor.execute(statement)
+                tables.append(connection.introspection.table_name_converter(model._meta.db_table))
+                
 
 models.signals.post_syncdb.connect(post_syncdb_duplicator)
