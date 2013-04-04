7badea28f9511b8a01de8889eef11a855df2a7b1
Matthew Schinckel <matt@schinckel.net>
2013-04-05 01:55:06 +1030

    Use the future-proof way of getting the User class. Add some comments.

--- multi_schema/models.py
+++ multi_schema/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib import auth
 from django.db import models, connection, transaction
 from django.utils.translation import ugettext as _
 from django.core.validators import RegexValidator
@@ -8,17 +8,37 @@ from django.forms import ValidationError
 import ensure_installation
 import signals
 
+try:
+    User = auth.get_user_model()
+except:
+    # Was getting an error where auth.models could not be found.
+    import django.contrib.auth.models
+    User = auth.models.User
+    
+
+# This is a bit of fancy trickery to stick the property _is_schema_aware
+# on every model class, returning False, unless it has been explicitly
+# set to True in the model definition (see base.py for examples).
+
 class ClassProperty(property):
     def __get__(self, cls, owner):
         return self.fget.__get__(None, owner)()
 
 def is_schema_aware(cls):
     return cls._meta.auto_created and cls._meta.auto_created._is_schema_aware
-    
+
 models.Model._is_schema_aware = ClassProperty(classmethod(is_schema_aware))
 
 
 class Schema(models.Model):
+    """
+    The Schema model provides an abstraction for a Postgres schema.
+    
+    It will take care of creating a cloned copy of the template_schema
+    when it is created, and also has the ability to activate and deactivate
+    itself (at the start and end of the request cycle would be a good plan).
+    
+    """
     name = models.CharField(max_length=128, help_text=_(u'The display name of the schema.'))
     schema = models.CharField(max_length=36, unique=True,
         validators=[RegexValidator(
@@ -63,9 +83,17 @@ class Schema(models.Model):
         (cursor or connection.cursor()).execute('SET search_path TO public')
         signals.schema_post_activate.send(sender=self, schema=self.schema)
 
+# An in-memory only template schema.
 template_schema = Schema(name="Template Schema", schema="__template__")
 
 class UserSchema(models.Model):
+    """
+    A relationship between a User and a Schema. A User may be able to
+    access the data from many schemata, and in that case should be
+    provided with tools to do so.
+    
+    See templates/multi_schema/change_schema.html for an example.
+    """
     user = models.ForeignKey(User, related_name='schemata')
     schema = models.ForeignKey(Schema, related_name='users')
     
