437ed5b567764e7cfe25dcac3799b754fe10cd0c
Matthew Schinckel <matt@schinckel.net>
2013-04-04 22:41:15 +1030

    Refactor syncdb replacement. Clone existing schemata if they don't exist at syncdb time.

--- multi_schema/management/commands/syncdb.py
+++ multi_schema/management/commands/syncdb.py
@@ -6,16 +6,19 @@ try:
 except ImportError:
     pass
 
+from ...models import Schema, template_schema
+
 class Command(syncdb.Command):
     def handle_noargs(self, **options):
-        cursor = connection.cursor()        
         # Ensure we have a __template__ schema.
-        cursor.execute("SELECT schema_name FROM information_schema.schemata WHERE schema_name = '__template__';")
-        if not cursor.fetchone():
-            cursor.execute("CREATE SCHEMA __template__;")
-        transaction.commit_unless_managed()
+        template_schema.create_schema()
         
         # Set the search path, so we find created models correctly
+        cursor = connection.cursor()
         cursor.execute("SET search_path TO public,__template__;")
         
         super(Command, self).handle_noargs(**options)
+        
+        # Ensure all existing schemata exist (in case we imported them using loaddata or something)
+        for schema in Schema.objects.all():
+            schema.create_schema()
