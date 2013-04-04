07351de021e98feb2340eb2efd8b6cb0a6163712
Matthew Schinckel <matt@schinckel.net>
2013-04-05 01:53:23 +1030

    Ensure that admin logs are tagged with the schema of the object if applicable. This makes links for these work in the admin.

--- multi_schema/ensure_installation.py
+++ multi_schema/ensure_installation.py
@@ -27,3 +27,37 @@ if MULTI_SCHEMA_MIDDLEWARE not in settings.MIDDLEWARE_CLASSES:
 if 'south' in settings.INSTALLED_APPS:
     if settings.INSTALLED_APPS.index('south') > settings.INSTALLED_APPS.index('multi_schema'):
         raise ImproperlyConfigured('You must have "south" in INSTALLED_APPS before "multi_schema".')
+
+if 'django.contrib.admin' in settings.INSTALLED_APPS:
+    # Patch LogEntry to store reference to Schema if applicable.
+    # We will assume that the LogEntry table does not exist...
+    from django.contrib.admin.models import LogEntry
+    from django.db import models
+    from django.dispatch import receiver
+    
+    LogEntry.add_to_class(
+        'object_schema', 
+        models.ForeignKey('multi_schema.Schema', blank=True, null=True)
+    )
+    
+    # Now, when we have an object that gets saved in the admin, we
+    # want to store the schema in the log, ...
+    @receiver(models.signals.pre_save, sender=LogEntry)
+    def update_object_schema(sender, instance, **kwargs):
+        obj = instance.get_edited_object()
+        if obj._is_schema_aware:
+            from schema import get_schema
+            instance.object_schema = get_schema()
+            
+    
+    # So we can add that bit to the url, and have links in the admin
+    # that will automatically change the schema for us.
+    get_admin_url = LogEntry.get_admin_url
+    
+    def new_get_admin_url(self):
+        if self.object_schema_id:
+            return get_admin_url(self) + '?__schema=%s' % self.object_schema_id
+        
+        return get_admin_url(self)
+    
+    LogEntry.get_admin_url = new_get_admin_url
