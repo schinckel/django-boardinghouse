7120b1ae45f85e246cce50e33f2ff4d79bbeabbb
Matthew Schinckel <matt@schinckel.net>
2013-04-05 17:09:53 +1030

    Rework the middleware, so it works a whole lot better. Hide the __schema=value as much from the user as possible. Provide alternative ways of changing the schema. (url, header).

--- multi_schema/middleware.py
+++ multi_schema/middleware.py
@@ -10,24 +10,56 @@ Otherwise, set the schema to the one associated with the logged in user.
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.utils.translation import ugettext as _
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
 from models import Schema
 
-def activate_schema(request, available_schemata):
-    if '__schema' in request.GET:
-        request.session['schema'] = request.GET['__schema']
-    if 'schema' in request.session:
+
+def activate_schema(available_schemata, session):
+    """
+    Activate the session's schema
+    """
+    if session.get('schema', None):
         try:
-            available_schemata.get(pk=request.session['schema']).activate()
+            available_schemata.get(pk=session['schema']).activate()
         except ObjectDoesNotExist:
-            messages.add_message(request, messages.WARNING, 
-                _(u'Unable to find Schema matching query: %s' % request.session['schema'])
+            logging.warning(
+                u'Unable to find Schema matching query: %s' % session['schema']
             )
-            request.session.pop('schema')
+            session.pop('schema')
         else:
-            return None
+            return True
 
 class SchemaMiddleware:
+    """
+    Middleware to set the postgres schema for the current request.
+    
+    The schema that will be used is stored in the session. A lookup will
+    occur (but this could easily be cached) on each request.
+    
+    To change schema, simply request a page with a querystring of:
+    
+        https://example.com/page/?__schema=<schema-name>
+    
+    The schema will be changed (or cleared, if this user cannot view 
+    that schema), and the page will be re-loaded (if it was a GET).
+    
+    Alternatively, you may add a request header:
+    
+        X-Change-Schema: <schema-name>
+    
+    This will not cause a redirect to the same page without query string. It
+    is the preferred method, because of that.
+    
+    There is also an injected url route:
+    
+        https://example.com/__change_schema__/<schema-name>/
+    
+    This is designed to be used from AJAX requests, as it returns a status
+    code (and a short message) about the schema change request.
+    
+    """
     def process_request(self, request):
         if request.user.is_anonymous():
             return None
@@ -35,10 +67,40 @@ class SchemaMiddleware:
             available_schemata = Schema.objects
         elif request.user.schemata.exists():
             available_schemata = request.user.schemata
-            
-        activate_schema(request, available_schemata)
+        
+        if request.path.startswith('/__change_schema__/'):
+            request.session['schema'] = request.path.split('/')[2]
+            if not request.session['schema']:
+                return HttpResponse('No schema selected: missing schema value', status=404)
+            if activate_schema(available_schemata, request.session):
+                return HttpResponse('Schema changed')
+            return HttpResponse('Unable to change schema', status=403)
+        elif request.GET.get('__schema', None):
+            request.session['schema'] = request.GET['__schema']
+            if request.method == "GET":
+                data = request.GET.copy()
+                data.pop('__schema')
+                if data:
+                    return redirect(request.path + '?' + data.urlencode())
+                return redirect(request.path)
+        elif 'HTTP_X_CHANGE_SCHEMA' in request.META:
+            request.session['schema'] = request.META['HTTP_X_CHANGE_SCHEMA']
+        
+        activate_schema(available_schemata, request.session)
+
+        
         
     def process_template_response(self, request, response):
+        """
+        Inject some context variables into the context for page
+        rendering. This means we can get access to the schema
+        information on each page, and use a schema switcher, as
+        can be found in ``multi_schema/templates/change_schema.html``.
+        
+        It would be nice to prefix them with underscores, but then we
+        would be unable to access them in the template.
+        """
+        
         if request.user.is_anonymous():
             return response
         
@@ -52,7 +114,7 @@ class SchemaMiddleware:
         
         response.context_data['schemata'] = available_schemata
         
-        if 'schema' in request.session:
+        if request.session.get('schema', None):
             response.context_data['selected_schema'] = request.session['schema']
         
         return response
