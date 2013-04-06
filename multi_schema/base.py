b117dd0e829c22994ecbb3d88968ad1012868de2
Matthew Schinckel <matt@schinckel.net>
2013-04-06 23:11:53 +1030

    Initial version of a Mixin/Manager that will query across schemata.

--- multi_schema/base.py
+++ multi_schema/base.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+from models import Schema
+
+class MultiSchemaMixin(object):
+    def from_schemata(self, *schemata):
+        """
+        Perform these queries across several schemata.
+        """
+        qs = getattr(self, 'get_queryset', self.get_query_set)()
+        query = str(qs.query)
+        
+        if len(schemata) == 1 and hasattr(schemata[0], 'filter'):
+            schemata = schemata[0]
+        
+        multi_query = [
+            query.replace('FROM "', 'FROM "%s"."' % schema.schema) for schema in schemata
+        ]
+        
+        return self.raw(" UNION ALL ".join(multi_query))
+
+class MultiSchemaManager(MultiSchemaMixin, models.Manager):
+    pass
+
 class SchemaAware(object):
     _is_schema_aware = True
 
