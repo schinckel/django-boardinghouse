2c1600adf5a9b96d1f9ada1ea374c3a7c0348adf
Matthew Schinckel <matt@schinckel.net>
2013-04-15 21:21:34 +0930

    Add tests for dumpdata command.

--- multi_schema/tests/test_management_commands.py
+++ multi_schema/tests/test_management_commands.py
@@ -1,3 +1,8 @@
+import unittest
+import sys
+from cStringIO import StringIO
+from contextlib import contextmanager
+
 from django.core.management import call_command
 from django.core.management.base import CommandError
 from django.db import connection, DatabaseError
@@ -10,6 +15,14 @@ from .models import AwareModel, NaiveModel
 
 SCHEMA_QUERY = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = %s"
 
+@contextmanager
+def capture(command, *args, **kwargs):
+    out, sys.stdout = sys.stdout, StringIO()
+    command(*args, **kwargs)
+    sys.stdout.seek(0)
+    yield sys.stdout.read()
+    sys.stdout = out
+
 class TestLoadData(TestCase):
     def test_invalid_schema_causes_error(self):
         with self.assertRaises(CommandError):
@@ -18,7 +31,8 @@ class TestLoadData(TestCase):
     def test_loading_schemata_creates_pg_schemata(self):
         self.assertEquals(0, Schema.objects.count())
         # Need to use commit=False, else the data will be in a different transaction.
-        call_command('loaddata', 'multi_schema/tests/fixtures/schemata.json', commit=False)
+        with capture(call_command, 'loaddata', 'multi_schema/tests/fixtures/schemata.json', commit=False) as output:
+            self.assertEquals('Installed 2 object(s) from 1 fixture(s)\n', output)
         self.assertEquals(2, Schema.objects.count())
         Schema.objects.all()[0].activate()
         self.assertTrue(get_schema())
@@ -30,13 +44,15 @@ class TestLoadData(TestCase):
             
 
     def test_loading_naive_data_does_not_require_schema_arg(self):
-        call_command('loaddata', 'multi_schema/tests/fixtures/naive.json', commit=False)
+        with capture(call_command, 'loaddata', 'multi_schema/tests/fixtures/naive.json', commit=False) as output:
+            self.assertEquals('Installed 2 object(s) from 1 fixture(s)\n', output)
         NaiveModel.objects.get(name="naive1")
     
     
     def test_loading_naive_data_works_with_schema_passed_in(self):
         Schema.objects.create(name='a', schema='a')
-        call_command('loaddata', 'multi_schema/tests/fixtures/naive.json', schema='a', commit=False)
+        with capture(call_command, 'loaddata', 'multi_schema/tests/fixtures/naive.json', schema='a', commit=False) as output:
+            self.assertEquals('Installed 2 object(s) from 1 fixture(s)\n', output)
         NaiveModel.objects.get(name="naive1")
     
     
@@ -46,10 +62,33 @@ class TestLoadData(TestCase):
     
     def test_loading_aware_data_works(self):
         Schema.objects.mass_create('a', 'b')
-        call_command('loaddata', 'multi_schema/tests/fixtures/aware.json', schema='a', commit=False)
+        with capture(call_command, 'loaddata', 'multi_schema/tests/fixtures/aware.json', schema='a', commit=False) as output:
+            self.assertEquals('Installed 2 object(s) from 1 fixture(s)\n', output)
         
         Schema.objects.get(schema='a').activate()
         AwareModel.objects.get(name='aware1')
         
         Schema.objects.get(pk='b').activate()
         self.assertRaises(AwareModel.DoesNotExist, AwareModel.objects.get, name='aware1')
+    
+    @unittest.expectedFailure
+    def test_loading_data_containing_schema_data_works(self):
+        self.assertTrue(False)
+
+class TestDumpData(TestCase):
+    def test_invalid_schema_raises_exception(self):
+        with self.assertRaises(CommandError):
+            call_command('dumpdata', 'multi_schema', schema='foo')
+            
+    def test_dumpdata_on_naive_models_does_not_require_schema(self):
+        with capture(call_command, 'dumpdata', 'multi_schema') as output:
+            self.assertEquals('[]', output)
+    
+    def test_dumpdata_on_aware_model(self):
+        Schema.objects.mass_create('a', 'b')
+        Schema.objects.get(schema='a').activate()
+        AwareModel.objects.create(name='foo')
+        with capture(call_command, 'dumpdata', 'multi_schema', schema='a') as output:
+            self.assertIn('{"status": false, "name": "foo"}', output)
+        with capture(call_command, 'dumpdata', 'multi_schema', schema='b') as output:
+            self.assertNotIn('{"status": false, "name": "foo"}', output)
