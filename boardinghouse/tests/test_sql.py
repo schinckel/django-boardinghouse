2a7eecbf55f5cc00bed76a70990946309baa2baa
Matthew Schinckel <matt@schinckel.net>
2014-04-23 17:27:57 +0930

    Make test work with 1.7

--- boardinghouse/tests/test_sql.py
+++ boardinghouse/tests/test_sql.py
@@ -4,7 +4,7 @@ Tests for the RAW sql functions.
 
 from django.conf import settings
 from django.test import TestCase
-from django.db.models import connection
+from django.db import connection
 
 from boardinghouse.models import Schema
 
@@ -13,4 +13,4 @@ class TestRejectSchemaColumnChange(TestCase):
         Schema.objects.mass_create('a')
         cursor = connection.cursor()
         UPDATE = "UPDATE boardinghouse_schema SET schema='foo' WHERE schema='a'"
-        self.assertRaises(Exception, cursor.execute, UPDATE)        
+        self.assertRaises(Exception, cursor.execute, UPDATE)
