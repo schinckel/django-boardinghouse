2ce1f562b03f9ff73fc650982c14b8c23269938e
Matthew Schinckel <matt@schinckel.net>
2014-07-10 19:15:31 +0930

    Tweak on the badge generation script.

--- badges.sh
+++ badges.sh
@@ -2,11 +2,11 @@
 
 COV=$(coverage report | tail -n 1 | rev | cut -f 1 -d " " | rev)
 
-if [[ $COV < "50%" ]] ; then
+if [[ $COV < '50%' ]] ; then
   COLOUR=red;
-elif [[ $COV < "75%" ]] ; then
+elif [[ $COV < '75%' ]] ; then
   COLOUR=orange ;
-elif [[ $COV < "90%" ]] ; then
+elif [[ $COV < '90%' ]] ; then
   COLOUR=yellow ;
 else
   COLOUR=brightgreen ;
