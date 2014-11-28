5e0b1980656c1bc9dca3eda6c09dd75b660e6bb0
Matthew Schinckel <matt@schinckel.net>
2014-11-29 10:06:28 +1030

    Find subpackages when packaging.
    Resolves #17.

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import boardinghouse
 
 setup(
-    name = "django-boardinghouse",
-    version = boardinghouse.__version__,
-    description = "Postgres schema support in django.",
-    url = "http://hg.schinckel.net/django-boardinghouse",
-    author = "Matthew Schinckel",
-    author_email = "matt@schinckel.net",
-    packages = [
-        "boardinghouse",
-    ],
+    name="django-boardinghouse",
+    version=boardinghouse.__version__,
+    description="Postgres schema support in django.",
+    url="http://hg.schinckel.net/django-boardinghouse",
+    author="Matthew Schinckel",
+    author_email="matt@schinckel.net",
+    packages=find_packages(),
     include_package_data=True,
-    install_requires = [
+    install_requires=[
         'django',
-        #'psycopg2', # or psycopg2cffi
+        # 'psycopg2',  # or psycopg2cffi under pypy
     ],
-    classifiers = [
+    classifiers=[
         'Programming Language :: Python',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
