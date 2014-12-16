8b780c7a7d5fd8a488055333b69935279aeed748
Matthew Schinckel <matt@schinckel.net>
2014-12-16 22:42:23 +1030

    Bump release number.

--- boardinghouse/__init__.py
+++ boardinghouse/__init__.py
@@ -10,7 +10,7 @@ See full documentation at: http://django-boardinghouse.readthedocs.org
 from __future__ import unicode_literals
 
 __version__ = '0.3'
-__release__ = '0.3a1'
+__release__ = '0.3a2'
 
 
 default_app_config = 'boardinghouse.apps.BoardingHouseConfig'
