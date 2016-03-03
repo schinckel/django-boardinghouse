6d491cb9b8c7e690cc7664f963a5c5832d13f66e
Matthew Schinckel <matt@schinckel.net>
2016-03-03 23:06:42 +1030

    Now working on next version.

--- boardinghouse/__init__.py
+++ boardinghouse/__init__.py
@@ -9,8 +9,8 @@ See full documentation at: http://django-boardinghouse.readthedocs.org
 
 from __future__ import unicode_literals
 
-__version__ = '0.3.5'
-__release__ = '0.3.5'
+__version__ = '0.3.6'
+__release__ = '0.3.6.dev'
 
 
 default_app_config = 'boardinghouse.apps.BoardingHouseConfig'
