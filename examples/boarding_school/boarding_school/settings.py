554c8306a6cbb55ddd6842028efff4ab24ae55ef
Matthew Schinckel <matt@schinckel.net>
2016-05-07 11:45:12 +0930

    Remove contrib.groups from example project for now.

--- examples/boarding_school/boarding_school/settings.py
+++ examples/boarding_school/boarding_school/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'boardinghouse',
     'boardinghouse.contrib.template',
+    # 'boardinghouse.contrib.groups',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
