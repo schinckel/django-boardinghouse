f78bd029ad19300ae2965ec6d74ec4a030623a0a
Matthew Schinckel <matt@schinckel.net>
2016-05-09 20:21:45 +0930

    Message user when templates created in admin.
    References #10.
    
    --HG--
    branch : contrib/demo

--- boardinghouse/contrib/template/admin.py
+++ boardinghouse/contrib/template/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
 from .models import SchemaTemplate
 
@@ -7,7 +8,18 @@ admin.site.register(SchemaTemplate)
 
 # Inject an action into the registered ModelAdmin for our Schema model.
 def create_template_from_schema(modeladmin, request, queryset):
+    created = []
     for schema in queryset:
         template = SchemaTemplate(name=schema.name)
         template._clone = schema.schema
         template.save()
+        created.append(template.name)
+
+    if len(created) == 1:
+        modeladmin.message_user(request,
+                                message=_('Created one new template: "{}"').format(*created),
+                                level='success')
+    elif len(created) > 1:
+        modeladmin.message_user(request,
+                                message=_('Created {} new templates: {}').format(len(created), ', '.join(created)),
+                                level='success')
