5896c983b7e95beee948561bcfff3a4af10bc288
Matthew Schinckel <matt@schinckel.net>
2017-04-22 22:08:07 +0930

    Remove tox-pyenv plugin
    --HG--
    branch : django-110-only

--- pyenv.sh
+++ pyenv.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-export PYENV_ROOT="${HOME}/pyenv"
-mkdir -p "${PYENV_ROOT}"
-wget https://github.com/yyuu/pyenv/archive/v1.0.10.tar.gz
-tar -xaf v1.0.10.tar.gz --strip-components=1 --directory="${PYENV_ROOT}"
-export PATH="${PYENV_ROOT}/bin:${PATH}"
-export PYENV_ROOT
+
+if [ ! -d "${PYENV_ROOT}" ]; then
+  mkdir -p "${PYENV_ROOT}"
+  wget https://github.com/yyuu/pyenv/archive/v1.0.10.tar.gz
+  tar -xaf v1.0.10.tar.gz --strip-components=1 --directory="${PYENV_ROOT}"
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  export PYENV_ROOT
+fi
 
 eval "$(pyenv init -)"
 pyenv install --skip-existing 3.6.1
