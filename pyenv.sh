330f2104939fa08fe7cd6f49347ef43054888491
Matthew Schinckel <matt@schinckel.net>
2017-04-22 21:57:55 +0930

    Fix typo
    --HG--
    branch : django-110-only

--- pyenv.sh
+++ pyenv.sh
@@ -2,11 +2,14 @@
 export PYENV_ROOT="${HOME}/pyenv"
 mkdir -p "${PYENV_ROOT}"
 wget https://github.com/yyuu/pyenv/archive/v1.0.10.tar.gz
-tar -xaf v1.0.10.tar.gz --strip-components=1 --directory= "${PYENV_ROOT}"
+tar -xaf v1.0.10.tar.gz --strip-components=1 --directory="${PYENV_ROOT}"
 export PATH="${PYENV_ROOT}/bin:${PATH}"
 export PYENV_ROOT
 
 eval "$(pyenv init -)"
-pyenv install 3.6.1
-pyenv install 3.5.3
-pyenv local system 3.6.1 3.5.3
+pyenv install --skip-existing 3.6.1
+pyenv install --skip-existing 3.5.3
+pyenv global system 3.6.1 3.5.3
+
+python --version
+pyenv version
